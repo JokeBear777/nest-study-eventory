b53e0e6991c09c435df0e87bf18ae4ed47e000f9
Jokebear777 <korae9955@naver.com>
2024-12-01 15:42:08 +0900

    1

--- src/club/club.controller.ts
+++ src/club/club.controller.ts
@@ -4,7 +4,6 @@ import { JwtAuthGuard } from "src/auth/guard/jwt-auth.guard";
 import { CurrentUser } from "src/auth/decorator/user.decorator";
 import { UserBaseInfo } from "src/auth/type/user-base-info.type";
 import { CreateClubPayload } from "./payload/create-club-payload";
-import { PutUpdateClubPayload } from "./payload/put-update-club-payload";
 import { ClubService } from "./club.service";
 import { ClubDto } from "./dto/club.dto";
 
@@ -24,32 +23,6 @@ export class ClubController {
     @CurrentUser() user: UserBaseInfo,
   ): Promise<ClubDto> {
     return this.clubService.createClub(payload, user);
-  }
-
-  @Put(':clubId')
-  @UseGuards(JwtAuthGuard)
-  @ApiBearerAuth()
-  @ApiOperation({ summary: '클럽을 수정합니다' })
-  @ApiOkResponse({ type: ClubDto })
-  async putUpdateClub(
-    @Param('clubId', ParseIntPipe) clubId: number,
-    @Body() payload: PutUpdateClubPayload,
-    @CurrentUser() user: UserBaseInfo,
-  ): Promise<ClubDto> {
-    return this.clubService.putUpdateClub(clubId, payload, user);
-  }
-
-  @Delete(':clubId')
-  @UseGuards(JwtAuthGuard)
-  @ApiBearerAuth()
-  @HttpCode(204)
-  @ApiOperation({ summary: '클럽을 삭제합니다' })
-  @ApiNoContentResponse()
-  async deleteClub(
-    @Param('clubId', ParseIntPipe) clubId: number,
-    @CurrentUser() user: UserBaseInfo,
-  ): Promise<void> {
-    return this.clubService.deleteClub(clubId, user);
   }
 
 }
--- src/club/club.repository.ts
+++ src/club/club.repository.ts
@@ -2,7 +2,6 @@ import { Injectable } from "@nestjs/common";
 import { CreateClubData } from "./type/create-club-data";
 import { ClubData } from "./type/club-data";
 import { PrismaService } from "src/common/services/prisma.service";
-import { UpdateClubData } from "./type/update-club-data";
 import { Status } from "@prisma/client";
 
 @Injectable()
@@ -32,75 +31,5 @@ export class ClubRepository {
       },
     });
   }
-
-  async getClubHeadCount(clubId: number): Promise<number> {
-    return this.prisma.clubMember.count({
-      where: {
-        clubId: clubId,
-        user: {
-          deletedAt: null,
-        },
-        status: {
-          not: Status.PENDING,
-        },
-      },
-    });
-  }
-
-  async getHostIdByClubId(clubId: number): Promise<number | null> {
-    const result = await this.prisma.club.findUnique({
-      where: {
-        id: clubId,
-      },
-      select: {
-        hostId: true,
-      },
-    });
-
-    return result?.hostId ?? null;
-  }
-
-  async updateClub(clubId: number, data: UpdateClubData): Promise<ClubData> {
-    return this.prisma.club.update({
-      where: {
-        id: clubId,
-      },
-      data: {
-        name: data.name,
-        description: data.description,
-        maxPeople: data.maxPeople,
-      },
-      select: {
-        id: true,
-        hostId: true,
-        name: true,
-        description: true,
-        maxPeople: true,
-      },
-    });
-  }
-
-  async getClubById(clubId: number): Promise<ClubData | null> {
-    return this.prisma.club.findUnique({
-      where: {
-        id: clubId,
-      },
-    });
-  }
-
-  async deleteClub(clubId: number): Promise<void> {
-    await this.prisma.clubMember.deleteMany({
-      where: {
-        clubId: clubId,
-      },
-    });
-
-    await this.prisma.club.delete({
-      where: {
-        id: clubId,
-      },
-    });
-  }
-
 
 }
